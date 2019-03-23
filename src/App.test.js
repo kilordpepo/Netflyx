a132969c6827d1f11f5fba042e60715b378db250
Carlos Valero <kilordpepo@gmail.com>
2019-03-23 11:30:15 -0400

    Fixing compose redux, initial test body

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { render, fireEvent, cleanup } from "react-testing-library";
+import configureStore from "./store";
+import SongsList from "./components/list";
+afterEach(cleanup);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// this is a handy function that I normally make available for all my tests
+// that deal with connected components.
+// you can provide initialState or the entire store that the ui is rendered with
+function renderWithRedux(ui, { initialState, store = configureStore() } = {}) {
+  return {
+    ...render(<Provider store={store}>{ui}</Provider>),
+    // adding `store` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    store
+  };
+}
+
+test("can render with redux with defaults", () => {
+  const { getByTestId, getByText } = renderWithRedux(<SongsList />);
+  fireEvent.click(getByText("+"));
+  expect(getByTestId("count-value").textContent).toBe("1");
 });
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,11 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "../reducers/";
+import thunk from "redux-thunk";
 
-import {createStore, applyMiddleware} from 'redux';
-import rootReducer from '../reducers/';
-import thunk from 'redux-thunk';
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    composeEnhancer(applyMiddleware(thunk))
-  );
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 }
